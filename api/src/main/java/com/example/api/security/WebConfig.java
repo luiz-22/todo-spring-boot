8fc30f826eda59394fdcdda796d6d5f004f34a4f
Luis <luismanlaz@hotmail.com>
2024-11-03 20:46:10 -0300

    03/11/2024

--- api/src/main/java/com/example/api/security/WebConfig.java
+++ api/src/main/java/com/example/api/security/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        //.allowedOrigins("http://localhost:5173")
                         .allowedOrigins("https://todo-spring-boot-eq0m.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
